68465879972d8003be2cbe76379f0794a049b411
jicu mollick <jikumollick82@gmail.com>
2022-05-05 10:41:45 +0600

    React Router  And Page Setup Done

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Inventory from "./components/Inventory/Inventory";
+import ManageItems from "./components/ManageItems/ManageItems";
+import AddItems from "./components/AddItems/AddItems";
+import MyItems from "./components/MyItems/MyItems";
+import Login from "./components/Login/Login";
+import Blogs from "./components/Blogs/Blogs";
+import About from "./components/About/About";
+import NotFound from "./components/NotFound/NotFound";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/manageItems" element={<ManageItems />} />
+        <Route path="/addItems" element={<AddItems />} />
+        <Route path="/myItems" element={<MyItems />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/notFound" element={<NotFound />} />
+
+        {/* <Route path="about" element={<About />} /> */}
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Blogs/Blogs.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Blogs = () => {
+  return (
+    <div>
+      <h2> I am Blogs</h2>
+    </div>
+  );
+};
+
+export default Blogs;
