ff040c0af65089bf351788e58d788a4abedb7898
jicu mollick <jikumollick82@gmail.com>
2022-05-06 01:33:35 +0600

    Footer section done

---
+++ src/components/Home/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-dark text-light">
+      <p className="text-center py-3">
+        {" "}
+        <span> © King Inventory 2022 , All Right Reserved </span>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Contact from "./Contact/Contact";
+import Footer from "./Footer/Footer";
 
 const Home = () => {
   return (
     <div>
       <Contact></Contact>
+      <Footer></Footer>
     </div>
   );
 };
